9d6e9b89f499cec9cdf6c1f356f18a5b1542948c
Avishi Gan <avishiganepola@gmail.com>
2022-07-10 10:42:26 +0530

    Add tutorial 2 scala codes (re)

---
+++ Q_1_2_3.scala
@@ -0,0 +1,14 @@
+object Q1,2,3 extends App{
+    var k, i, j: Int = 2
+    var m, n: Int = 5
+    var f: Float = 12.0f
+    var g: Float = 4.0f
+    var c: Char = 'X'
+
+    println (k + 12 * m);
+    println (m / j);
+    println (n % j);
+    println (m / j * j);
+    println (f + 10*5 +g);
+    println (++i * n);
+}
